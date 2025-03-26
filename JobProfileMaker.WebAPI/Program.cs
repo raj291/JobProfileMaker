f94dede4048edaabf87281f73408adf244b90a1c
Raj Mahadik <raj.mahadik2998@gmail.com>
2025-03-26 15:04:33 -0400

    Dependency Injection for all Services and Repository

--- JobProfileMaker.WebAPI/Program.cs
+++ JobProfileMaker.WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using JobProfileMaker.core.Contracts.EntityServices;
+using JobProfileMaker.core.Contracts.Repository;
 using JobProfileMaker.Infrastructure.Data;
+using JobProfileMaker.Infrastructure.Repository;
+using JobProfileMaker.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +18,14 @@ builder.Services.AddDbContext<JobProfileMakerDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProfileMaker"));
 });
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<ICandidateRepository, CandidateRepositoryAsync>();
+builder.Services.AddScoped<ICandidateService, CandidateServiceAsync>();
+builder.Services.AddScoped<IEducationRepository, EducationRepositoryAsync>();
+builder.Services.AddScoped<IEducationService, EducationServiceAsync>();
+builder.Services.AddScoped<IExperienceRepository, ExperienceRepositoryAsync>();
+builder.Services.AddScoped<IExperienceService, ExperienceServiceAsync>();
+builder.Services.AddScoped<ISkillRepository, SkillRepositoryAsync>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
